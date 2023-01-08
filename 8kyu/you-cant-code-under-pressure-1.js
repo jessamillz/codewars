a31f93a38400c33f127a9c879b7347883966a412
jessamillz <j_miller2007@yahoo.com>
2023-01-08 12:59:32 -0500

    added 8kyu you-cant-code-under-pressure-1

---
+++ 8kyu/you-cant-code-under-pressure-1.js
@@ -0,0 +1,13 @@
+// Code as fast as you can! You need to double the integer and return it.
+//My Solution
+function doubleInteger(i) {
+  return i*2;
+}
+
+//Other solutions (not mine)
+const doubleInteger = i => 2 * i;
+
+function doubleInteger(i) {
+  i *= 2;
+  return i;
+}
