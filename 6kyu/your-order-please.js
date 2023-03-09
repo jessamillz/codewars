63650a5eacf9aa80fb83d4bc20376aa226f16ed8
jessamillz <j_miller2007@yahoo.com>
2023-03-09 12:46:10 -0500

    added other solutions to your-order-please

--- 6kyu/your-order-please.js
+++ 6kyu/your-order-please.js
@@ -39,3 +39,23 @@ console.log(order('4of Fo1r pe6ople g3ood th5e the2'));
 
 
 //* OTHER SOLUTIONS, NOT MINE *//
+
+function order(words){
+  
+    return words.split(' ').sort(function(a, b){
+        return a.match(/\d/) - b.match(/\d/);
+     }).join(' ');
+}
+
+function order(words){
+    var array = words.split(' ');
+    var sortedArray = [];
+    for(i = 0; i <= array.length; i++) {
+      for(j = 0; j < array.length; j++) {
+        if(array[j].indexOf(i) >= 0) {
+          sortedArray.push(array[j]);
+        }
+      }
+    }
+    return sortedArray.join(' ');
+}
